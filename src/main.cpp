60501c7090dcca0c64b83695ef1cc30ea9fd446f
Chris Chafe <4406287+cchafe@users.noreply.github.com>
2021-03-29 16:10:42 -0700

    working

---
+++ src/main.cpp
@@ -0,0 +1,197 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QLoggingCategory>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <jack/jack.h>
+jack_port_t *input_port;
+jack_port_t *output_port;
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int xctr;
+int ctr;
+jack_client_t *client;
+
+// Jack XRUN callback function, called
+// whenever there is a xrun.
+static int qjackctl_xrun_callback ( void * )
+{
+//    qDebug() << "hi";
+//    std::cout << ".";
+//    fprintf(stderr,".");
+    xctr++;
+    return 0;
+}
+void report()
+{
+    double PPS = 48000/32.0;
+    double tmp = (ctr/PPS);
+float fDspLoad = jack_cpu_load(client);
+qDebug() << xctr << "\t(" << (int)(tmp) << ")" << "\t(" << (xctr/tmp) << ")" << "\t" << fDspLoad;
+}
+
+int
+process (jack_nframes_t nframes, void *arg)
+{
+//    jack_default_audio_sample_t *in, *out;
+
+//    in = (jack_default_audio_sample_t *)jack_port_get_buffer (input_port, nframes);
+//    out =(jack_default_audio_sample_t *) jack_port_get_buffer (output_port, nframes);
+//    memcpy (out, in,
+//        sizeof (jack_default_audio_sample_t) * nframes);
+ctr++;
+//qDebug() << ctr;
+if (!(ctr%2000)) { report(); }
+    return 0;
+}
+
+void
+jack_shutdown (void *arg)
+{
+    exit (1);
+}
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    fprintf(stderr, "%s\n", localMsg.constData());
+    fflush(stderr);
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QLoggingCategory::setFilterRules(QStringLiteral("*.debug=true"));
+    qInstallMessageHandler(myMessageOutput);
+//    int
+//    main (int argc, char *argv[])
+    {
+        const char **ports;
+        const char *client_name = "simple";
+        const char *server_name = NULL;
+        jack_options_t options = JackNullOption;
+        jack_status_t status;
+
+        ctr=0;
+        xctr=0;
+
+        client = jack_client_open (client_name, options, &status, server_name);
+        if (client == NULL) {
+            fprintf (stderr, "jack_client_open() failed, "
+                 "status = 0x%2.0x\n", status);
+            if (status & JackServerFailed) {
+                fprintf (stderr, "Unable to connect to JACK server\n");
+            }
+            exit (1);
+        }
+        qDebug() << "simple";
+        fprintf (stderr, "JACK server started\n");
+        if (status & JackServerStarted) {
+            fprintf (stderr, "JACK server started\n");
+        }
+        if (status & JackNameNotUnique) {
+            client_name = jack_get_client_name(client);
+            fprintf (stderr, "unique name `%s' assigned\n", client_name);
+        }
+
+        jack_set_xrun_callback(client,
+            qjackctl_xrun_callback, 0);
+
+        /* tell the JACK server to call `process()' whenever
+           there is work to be done.
+        */
+
+        jack_set_process_callback (client, process, 0);
+
+        /* tell the JACK server to call `jack_shutdown()' if
+           it ever shuts down, either entirely, or if it
+           just decides to stop calling us.
+        */
+
+        jack_on_shutdown (client, jack_shutdown, 0);
+
+        /* display the current sample rate.
+         */
+
+        fprintf (stderr,"engine sample rate: %" PRIu32 "\n",
+            jack_get_sample_rate (client));
+
+        /* create two ports */
+
+        input_port = jack_port_register (client, "input",
+                         JACK_DEFAULT_AUDIO_TYPE,
+                         JackPortIsInput, 0);
+        output_port = jack_port_register (client, "output",
+                          JACK_DEFAULT_AUDIO_TYPE,
+                          JackPortIsOutput, 0);
+
+        if ((input_port == NULL) || (output_port == NULL)) {
+            fprintf(stderr, "no more JACK ports available\n");
+            exit (1);
+        }
+
+        /* Tell the JACK server that we are ready to roll.  Our
+         * process() callback will start running now. */
+
+        if (jack_activate (client)) {
+            fprintf (stderr, "cannot activate client");
+            exit (1);
+        }
+        qDebug() << "activated";
+
+        /* Connect the ports.  You can't do this before the client is
+         * activated, because we can't make connections to clients
+         * that aren't running.  Note the confusing (but necessary)
+         * orientation of the driver backend ports: playback ports are
+         * "input" to the backend, and capture ports are "output" from
+         * it.
+         */
+
+//        ports = jack_get_ports (client, NULL, NULL,
+//                    JackPortIsPhysical|JackPortIsOutput);
+//        if (ports == NULL) {
+//            fprintf(stderr, "no physical capture ports\n");
+//            exit (1);
+//        }
+
+//        if (jack_connect (client, ports[0], jack_port_name (input_port))) {
+//            fprintf (stderr, "cannot connect input ports\n");
+//        }
+
+//        free (ports);
+
+//        ports = jack_get_ports (client, NULL, NULL,
+//                    JackPortIsPhysical|JackPortIsInput);
+//        if (ports == NULL) {
+//            fprintf(stderr, "no physical playback ports\n");
+//            exit (1);
+//        }
+
+//        if (jack_connect (client, jack_port_name (output_port), ports[0])) {
+//            fprintf (stderr, "cannot connect output ports\n");
+//        }
+
+//        free (ports);
+
+        /* keep running until stopped by the user */
+
+//        sleep (-1);
+
+        /* this is never reached but if the program
+           had some other way to exit besides being killed,
+           they would be important to call.
+        */
+
+//        jack_client_close (client);
+//        exit (0);
+    }
+
+    return a.exec();
+}
